add1cac511208bc4fa3a12d43c5f63f579958291
JavierMestre96 <javiermestre96@gmail.com>
2019-05-22 10:19:24 +0200

    Add files via upload

---
+++ PracticaFinal/src/Herramienta.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+//Poner abstract de nuevo al acabar
+public abstract class Herramienta {
+    private final String marca;
+    private final String nombre;
+    private final String descripcion;
+    private double precio;
+
+    Herramienta(String marca, String nombre, String descripcion, double precio){
+        compruebaMarca(marca);
+        compruebaNombre(nombre);
+
+        this.marca=marca;
+        this.nombre=nombre;
+        this.descripcion=descripcion;
+        this.precio=precio;
+    }
+
+    public void compruebaMarca(String marca){
+        if ("".equals(marca)){
+            throw new IllegalArgumentException("Marca no válida");
+        }
+    }
+    public void compruebaNombre(String nombre){
+        if ("".equals(nombre)){
+            throw new IllegalArgumentException("Nombre no válido.");
+        }
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Herramienta{" +
+                "marca='" + marca + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
